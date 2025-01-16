092339126ad409a221a0250e11daeb0ffa01d6d1
Indhumathi-Chella <indhumathi.chella@aspiresys.com>
2025-01-16 15:09:07 +0530

    Updated version

---
+++ assets/js/conditionals.js
@@ -0,0 +1,94 @@
+ // 1. if...else with user input
+ let number = 5; 
+ if (number > 0) {
+   console.log("Positive number.");
+ } else if (number1 < 0) {
+   console.log("Negative number.");
+ } else {
+   console.log("Zero.");
+ }
+
+ // 2. if without else
+ let num = 4; 
+ if (num % 2 === 0) {
+   console.log("Even number.");
+ }
+
+ // 3. if...else if (Signal check)
+ let signal = "green"; 
+ if (signal === "red") {
+   console.log("Stop, the signal is red.");
+ } else if (signal === "yellow") {
+   console.log("Caution, the signal is yellow.");
+ } else if (signal === "green") {
+   console.log("Go, the signal is green.");
+ } else {
+   console.log("Unknown signal color.");
+ }
+
+ // 4. switch statement 
+ let input = false; // Can change 
+
+switch(input) {
+  case 1:
+    console.log("The value is number 1.");
+    break;
+  case true:
+    console.log("The value is boolean true.");
+    break;
+  case false:
+    console.log("The value is boolean false.");
+    break;
+    case "hi":
+    console.log("welcome Home!");
+    break;
+  default:
+    console.log("No match found.");
+}
+
+ 
+ // 5. Ternary operator (Check Age)
+ let age = 20; 
+ let resultText = age >= 18 ? "Adult" : "Minor";
+ console.log(resultText);
+
+ // 6. Logical AND (&&)  (Voting Eligibility)
+ let age2 = 20; 
+ let isCitizen = true; 
+ if (age2 >= 18 && isCitizen) {
+   console.log("You can vote.");
+ } else {
+   console.log("You cannot vote.");
+ }
+
+ // 7. Logical OR (||) (Check Relax)
+ let isWeekend = true; 
+ let isHoliday = false; 
+ if (isWeekend || isHoliday) {
+   console.log("You can relax!");
+ } else {
+   console.log("Time to work.");
+ }
+
+ // 8. Logical NOT (!)  (Check member)
+ let isMember = false; 
+
+ 
+ if (!isMember) {
+   console.log("You are not a member, no discount for you.");
+ } else {
+   console.log("You are a member, you get a discount!");
+ }
+ 
+ // 9. Nested if...else (Weather Check)
+ let weather = "sunny"; 
+ let temperature = 32; 
+ if (weather === "sunny") {
+   if (temperature > 30) {
+     console.log("Its sunny and hot!");
+   } else {
+     console.log("Its sunny but cool.");
+   }
+ } else {
+   console.log("Its not sunny.");
+ }
---
+++ assets/js/loops.js
@@ -0,0 +1,61 @@
+// 1. For Loop 
+console.log("For Loop Example: Sum of numbers from 1 to 10");
+
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+console.log(`Sum from 1 to 10 is: ${sum}`);
+
+// 2. While Loop 
+console.log("\nWhile Loop Example: Countdown from 5");
+
+let countdown = 5;
+while (countdown >= 0) {
+    console.log(`Countdown: ${countdown}`);
+    countdown--; 
+}
+
+// 3. Do...While Loop
+let count = 0;
+
+do {
+    console.log(count);  
+    count++;  
+} while (count < 5);  
+
+// 4. For...of Loop : display list of fruits
+console.log("\nFor...of Loop Example: Display a list of fruits");
+
+const fruits = ["Apple", "Banana", "Mango"];
+for (const fruit of fruits) {
+    console.log(`Fruit: ${fruit}`);
+}
+
+// 5. For...in Loop: display properties of object(User profile)
+console.log("\nFor...in Loop Example: Display User Profile");
+
+const userProfile = {
+    name: "John Doe",
+    age: 30,
+    city: "New York"
+};
+
+for (const key in userProfile) {
+    console.log(`${key}: ${userProfile[key]}`);
+}
+
+// 6. Break and Continue
+console.log("\nBreak and Continue Example: Control loop flow");
+
+for (let i = 1; i <= 6; i++) {
+    if (i === 4) {
+        console.log("Breaking at iteration " + i);
+        break; // Stop loop 
+    }
+    if (i === 2) {
+        console.log("Skipping iteration " + i);
+        continue; // Skip the current iteration 
+    }
+    console.log(`Iteration: ${i}`);
+}
